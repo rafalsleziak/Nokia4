f08c11a9a28f980d0f717ebdddee0b088dd5ec7d
maciejjaskolski <226242@student.pwr.wroc.pl>
2018-03-22 08:57:49 +0100

    frontend

--- Backend/models/device.js
+++ Backend/models/device.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -27,41 +27,3 @@ DeviceSchema
 });
 
 module.exports = mongoose.model('Device', DeviceSchema);
-=======
-'use strict'
-
-module.exports = (sequelize, DataTypes) => {
-  var device = sequelize.define('devices', {
-    deviceName: {
-      type: DataTypes.STRING,
-      validate: {
-        len:{
-          args: [1, 30],
-          msg: 'Device name must be at least 1 character long and less than 30 characters'
-        }
-      }
-    },
-    numLeft: {
-        type: DataTypes.INTEGER,
-        validate: {
-          isInt: {
-            args: true,
-            msg: 'Availavle devices number must be an integer'
-          },
-          min:{
-            args: 1,
-            msg: 'Available devices number must be greater than 0'
-          }
-        }
-      }
-  });
-
-  device.prototype.url = function() {
-    return "/"+this.id;
-  }
-
-
-  return device;
-
-};
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
--- Backend/routes/device.js
+++ Backend/routes/device.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 var Device = require('../models/device');
 //var device_controller = require('../controllers/deviceController.js');
 
@@ -25,15 +24,11 @@ console.log(req.body);
 });
 
 router.route('/:device_id')
-//The put method gives us the chance to update our comment based on the ID passed to the route
   .put(function(req, res) {
     Device.findById(req.params.device_id, function(err, device) {
       if (err) {        res.send(err); }
-      //setting the new author and text to whatever was changed. If nothing was changed
-      // we will not alter the field.
       (req.body.name) ? device.name = req.body.name : null;
       (req.body.numLeft) ? device.numLeft= req.body.numLeft : null;
-      //save comment
      device.save(function(err) {
         if (err)
           res.send(err);
@@ -41,43 +36,12 @@ router.route('/:device_id')
       });
     });
   })
-  //delete method for removing a comment from our database
   .delete(function(req, res) {
-    //selects the comment by its ID, then removes it.
     Device.remove({ _id: req.params.device_id }, function(err, device){
       if (err)
         res.send(err);
       res.json({ message: 'device has been deleted' })
     })
 });
-//router.get('/', function(req, res) {res.json({message: 'API connected'})})//('/all', device_controller.device_list);
-
-//router.get('/create', device_controller.device_create_get);
-//router.route.post('/', device_controller.device_create_post);
-
-//router.get('/:id', device_controller.device_detail);
-
-//router.route('/:device_id')
-//.delete(device_controller.device_delete);
-//router.post(':id/delete', device_controller.device_delete_post);
-
-//router.get(':id/update', device_controller.device_update_get);
-//router.post(':id/update', device_controller.device_update_post);
-=======
-var device_controller = require('../controllers/deviceController.js');
-
-router.get('/all', device_controller.device_list);
-
-router.get('/create', device_controller.device_create_get);
-router.post('/create', device_controller.device_create_post);
-
-router.get('/:id', device_controller.device_detail);
-
-router.get(':id/delete', device_controller.device_delete_get);
-router.post(':id/delete', device_controller.device_delete_post);
-
-router.get(':id/update', device_controller.device_update_get);
-router.post(':id/update', device_controller.device_update_post);
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
 
 module.exports = router;
