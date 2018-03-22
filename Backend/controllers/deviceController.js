136bb393f378cfda4f20178e6206f00778bd9b3a
maciejjaskolski <226242@student.pwr.wroc.pl>
2018-03-22 09:00:57 +0100

    frontend

--- Backend/controllers/deviceController.js
+++ Backend/controllers/deviceController.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
+
 var Device = require('../models/device');
-=======
-var models = require('../models/');
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
+
 
 const {body, validationResult} = require('express-validator/check');
 const {sanitizeBody} = require('express-validator/filter');
 
-
-<<<<<<< HEAD
 exports.index = function(req, res, next) {
   res.json({express: 'API works!'});
   console.log('hello there');
@@ -20,41 +16,30 @@ exports.device_list = function(req, res, next) {
        res.send(err);
      res.json(devices)
    });
-=======
 exports.index = function(req, res) {
   res.render('index');
 }
 
 exports.device_list = function(req, res) {
   models.devices.findAll({  }).then((result) => {
-    res.render('all', {title: 'Nokia Garage Home Page', device_list: result});
   });
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
 };
 
 exports.device_detail = function(req, res) {
     models.devices.findOne({where: {'id': req.params.id}}).then((result) =>{
-<<<<<<< HEAD
       //res.render('device_detail', {title: "Device details", device_detail: result});
       res.json(result);
   //  res.json({expess: 'hello there'});
-=======
-      res.render('device_detail', {title: "Device details", device_detail: result});
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
     });
 };
 
 exports.device_create_get = function(req, res) {
-<<<<<<< HEAD
 
-=======
-  res.render('form_device', {title: 'Create device'});
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
+
 }
 
 //no validation...yet
 exports.device_create_post = function(req, res) {
-<<<<<<< HEAD
   var device = new Device();
   (req.body.name) ? device.name = req.body.name : null;
   (req.body.numLeft) ? device.numLeft= req.body.numLeft : null;
@@ -72,7 +57,6 @@ exports.device_delete = function(req, res) {
       res.send(err);
     res.json({ message: 'device has been deleted' })
   });
-=======
   models.devices.create({
     deviceName: req.body.deviceName,
     numLeft: req.body.numLeft
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,12 +4,7 @@ var router = express.Router();
 var device_controller = require('../controllers/deviceController.js')
 
 /* GET home page. */
-<<<<<<< HEAD
-//router.get('/', function(req, res) {
-//  res.json({message: 'api connected!'});
-//})//device_controller.index);
-=======
+
 router.get('/', device_controller.index);
->>>>>>> b3144d031cc4cba1e14948c7a3938967670870db
 
 module.exports = router;
