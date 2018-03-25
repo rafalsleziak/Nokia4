b02e4d381222eb070d403e913b5ba4511f0c7662
maciejjaskolski <226242@student.pwr.wroc.pl>
2018-03-25 17:37:19 +0200

    data seed reservations

--- Backend/database.js
+++ Backend/database.js
@@ -1,5 +1,5 @@
 var DeviceItem = require('./models/device.js');
-
+var ReservationItem = require('./models/reservations.js');
 
 
   //mongoose.connection.db.dropDataBase();
@@ -12,8 +12,26 @@ var DeviceItem = require('./models/device.js');
     numLeft: 7
   }];
 
+  var seedReservations = [{
+    startDate: '2018-03-25T23:10:15.000Z',
+    endDate: '2018-03-25T23:11:00.000Z',
+    numOfPeople: 3,
+    option: 'WholeSpace'
+  },{
+    startDate: '2018-03-25T23:11:05.000Z',
+    endDate: '2018-03-25T23:18:00.000Z',
+    numOfPeople: 10,
+    option: 'MakerSpace'
+  }];
+
   DeviceItem.remove({}, ()=>{
     seedDevices.forEach(function(item){
       new DeviceItem(item).save();
+    })
+  })
+
+  ReservationItem.remove({}, ()=>{
+    seedReservations.forEach(function(item){
+      new ReservationItem(item).save();
     })
   })
